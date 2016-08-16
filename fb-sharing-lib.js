520be12c3ec9fff8966d465b2bb89b40e636e149
Yu-Che-Gao <s0258037@gm.csmu.edu.tw>
2016-08-16 20:51:19 +0800

    facebook sharing
    Signed-off-by: Yu-Che-Gao <s0258037@gm.csmu.edu.tw>

---
+++ fb-sharing-lib.js
@@ -0,0 +1,19 @@
+function parseGET() {
+    var strUrl = location.search;
+    var getPara, ParaVal;
+    var aryPara = [];
+
+    if (strUrl.indexOf("?") != -1) {
+        var getSearch = strUrl.split("?");
+        getPara = getSearch[1].split("&");
+        for (i = 0; i < getPara.length; i++) {
+            ParaVal = getPara[i].split("=");
+            aryPara.push(ParaVal[0]);
+            aryPara[ParaVal[0]] = ParaVal[1];
+        }
+        return aryPara;
+    }
+
+    console.log('There is not any GET request.');
+    return;
+}
